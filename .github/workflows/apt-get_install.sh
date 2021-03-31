dd4911a3b2dc974fd75ca02cf3c5eaacca62628c
cradle8810 <falcon.int50m@gmail.com>
2021-03-31 19:39:07 +0900

    Test

---
+++ .github/workflows/apt-get_install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# https://github.com/gpac/gpac/wiki/GPAC-Build-Guide-for-Linux
+apt-get update
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get install -y \
+  build-essential \
+  ccache \
+  cmake \
+  debhelper \
+  devscripts \
+  dpkg-dev \
+  dvb-apps \
+  fakeroot \
+  g++ \
+  git \
+  liba52-0.7.4-dev \
+  libasound2-dev \
+  libavcodec-dev \
+  libavdevice-dev \
+  libavformat-dev \
+  libavutil-dev \
+  libfaad-dev \
+  libfreetype6-dev \
+  libgl1-mesa-dev \
+  libjack-dev \
+  libjpeg62-dev \
+  libmad0-dev \
+  libogg-dev \
+  libpng-dev \
+  libpulse-dev \
+  libsdl2-dev \
+  libssl-dev \
+  libswscale-dev \
+  libtheora-dev \
+  libvorbis-dev \
+  libxv-dev \
+  libxvidcore-dev \
+  mesa-utils \
+  pkg-config \
+  scons \
+  x11proto-gl-dev \
+  x11proto-video-dev \
+  yasm \
+  zlib1g-dev
